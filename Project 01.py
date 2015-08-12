f4e11358733d98ddc2435f4df0eb4ed4393f33a7
samer477 <Benhameds@ymail.com>
2015-08-12 18:23:04 +0200

    Update Project 01.py

--- Project 01.py
+++ Project 01.py
@@ -1,4 +1,20 @@
 import random
+print" _    _      _                            _         ______ _   ____  _________ "
+print"| |  | |    | |                          | |        |  _  \ | | |  \/  || ___ \ "
+print"| |  | | ___| | ___ ___  _ __ ___   ___  | |_ ___   | | | | | | | .  . || |_/ /"
+print"| |/\| |/ _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  | | | | | | | |\/| ||  __/ "
+print"\  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |/ /| |_| | |  | || |    "
+print" \/  \/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/  |___/  \___/\_|  |_/\_|    "
+print""
+print"welcome, welcome, welcome!!! There you are... my name is Pewwwwwwdiepie"
+print"I was just kidding my name is DUMP :( i hate my name!!!!"
+print"Now, since you are here you can Discover the Universe More with Python..."
+print"And now I just told DUMP means what."
+print"Oh My God Devoloppers why are you rushing me!!! we're almost there... i know i talk to much but just ignore me. "
+print"Why do you also want me to say your names... ok!!! your name is Samer his name is Yassine her eh..eh..h.i I mean his name is Mehdi"
+print""
+print"---------------------------------------------------------------------------------------------------------------------------------------------"
+print""
 while 1!=2:
     Y = raw_input("the calculator: C\nthe game : G\ninformations about the solar system : I\nTest astro : T\nto quit : Q\n... ")
     if Y == "Q" or Y == "q":
@@ -15,28 +31,28 @@ while 1!=2:
                 print"_Densité moyenne (g/cm3): 5.6"
                 print"_Journée: 58j15h"
                 print"_Révolution: 88j"
-                print"_Distance au soleil (Mkm): 58"
+                print"_Distance au soleil (Mkm): 58\n----------------------------------------------------------------------------------------------------------------------------"
             elif P == "2":
                 print"_Masse comparé à la Terre: 0.82"
                 print"_Diamétre à l'équateur (Km): 12140"
                 print"_Densité moyenne (g/cm): 5.2"
                 print"_Journée: 243j"
                 print"_Révolution: 225j"
-                print"_Distance au Soleil (Mkm): 108"
+                print"_Distance au Soleil (Mkm): 108\n----------------------------------------------------------------------------------------------------------------------------"
             elif P == "3":
-                print"_Masse comparé à la Terre: 1\n_Diamétre à l'équateur(Km): 12760\n_Densité moyenne(g/cm3): 5.5\n_Journée: 23h56mn\n_Révolution: 365j6h\n_Distance au Soleil(Mkm): 150"
+                print"_Masse comparé à la Terre: 1\n_Diamétre à l'équateur(Km): 12760\n_Densité moyenne(g/cm3): 5.5\n_Journée: 23h56mn\n_Révolution: 365j6h\n_Distance au Soleil(Mkm): 150\n----------------------------------------------------------------------------------------------------------------------------"
             elif P == "4":
-                print"_Masse comparé à la Terre: 0.11\n_Diamètre à l'equateur: 6800\n_Densité moyenne (g/cm3): 3.9\n_Journée: 24h37mn\n_Révolution: 1an11m\n_Distance au Soleil(Mkm): 228"
+                print"_Masse comparé à la Terre: 0.11\n_Diamètre à l'equateur: 6800\n_Densité moyenne (g/cm3): 3.9\n_Journée: 24h37mn\n_Révolution: 1an11m\n_Distance au Soleil(Mkm): 228\n----------------------------------------------------------------------------------------------------------------------------"
             elif P == "5":
-                print"_Masse comparé à la Terre: 318\n_Diamétre à l'équteur: 143200\n_Densité moyenne(g/cm3): 1.3\n_Journée: 9h45mn\n_Révolution: 11an11m\n_Distance au Soleil(Mkm): 778"
+                print"_Masse comparé à la Terre: 318\n_Diamétre à l'équteur: 143200\n_Densité moyenne(g/cm3): 1.3\n_Journée: 9h45mn\n_Révolution: 11an11m\n_Distance au Soleil(Mkm): 778\n----------------------------------------------------------------------------------------------------------------------------"
             elif P == "6":
-                print"_Masse comparé à la Terre: 95\n_Diamétre à l'équateur: 12000\n_Densité moyenne(g/cm3): 0.7\n_Journée: 10h39mn\n_Révolution: 29a6m\n_Distance au Soleil(Mkm): 1400"
+                print"_Masse comparé à la Terre: 95\n_Diamétre à l'équateur: 12000\n_Densité moyenne(g/cm3): 0.7\n_Journée: 10h39mn\n_Révolution: 29a6m\n_Distance au Soleil(Mkm): 1400\n----------------------------------------------------------------------------------------------------------------------------"
             elif P == "7":
-                print"_Masse comparé à la Terre: 15\n_Diamétre à l'equateur: 52000\n_Densité moyenne(g/cm3): 1.2\n_Journée: 17h15mn\n_Révolution: 84an\n_Distance au Soleil(Mkm): 2870"
+                print"_Masse comparé à la Terre: 15\n_Diamétre à l'equateur: 52000\n_Densité moyenne(g/cm3): 1.2\n_Journée: 17h15mn\n_Révolution: 84an\n_Distance au Soleil(Mkm): 2870\n----------------------------------------------------------------------------------------------------------------------------"
             elif P == "8":
-                print"_Masse comparé à la Terre: 17\n_Diamétre à l'equateur: 50000\n_Densité moyenne(g/cm3): 1.7\n_Journée: 17h36mn\n_Révolution: 165an\n_Distance au Soleil(Mkm): 4500"
+                print"_Masse comparé à la Terre: 17\n_Diamétre à l'equateur: 50000\n_Densité moyenne(g/cm3): 1.7\n_Journée: 17h36mn\n_Révolution: 165an\n_Distance au Soleil(Mkm): 4500\n----------------------------------------------------------------------------------------------------------------------------"
             elif P == "9":
-                print"_Masse comparé à la Terre: 0.003\n_Diamétre à l'equateur: 2300\n_Densité moyenne(g/cm3): 2\n_Journée: 6j10h\n_Révolution: 248an\n_Distance au Soleil(Mkm): 5900"
+                print"_Masse comparé à la Terre: 0.003\n_Diamétre à l'equateur: 2300\n_Densité moyenne(g/cm3): 2\n_Journée: 6j10h\n_Révolution: 248an\n_Distance au Soleil(Mkm): 5900\n----------------------------------------------------------------------------------------------------------------------------"
     if Y == "T" or Y == "t":
         while 1!=2:
             print"you are now in the Test section"
@@ -47,29 +63,29 @@ while 1!=2:
                 print"Ok now you'll be asked 10 questions..."
                 L=0
                 K=0
-                o=raw_input("_1st of all who's the first human been that went to the moon?\n*Neil Armstrong[n]\n*Josef Fourier[j]\n*Stieve Woziniak[S]\n...")
-                if o == "n":
+                o=raw_input("_1st of all who's the first human been that went to the moon?\n*Neil Armstrong[1]\n*Josef Fourier[2]\n*Stieve Woziniak[3]\n...")
+                if o == "1":
                     print"Success you are right!\n-------------------------------------------------------------------------"
                     K+=1
                 else:
                     print"Are you sure? because it's the wrong answer the right one is :\nNeil Armstrong\n------------------------------------------------------------------------"
                     L+=1
-                o1=raw_input("_When the human been discovered Mars?\n*1378[a]\n*1786[a1]\n*1543[a2]\n...")
-                if o1 == "a2":
+                o1=raw_input("_When the human been discovered Mars?\n*1378[1]\n*1786[2]\n*1543[3]\n...")
+                if o1 == "3":
                     print"Success you are right!\n-------------------------------------------------------------------------"
                     K+=1
                 else:
                     print"Are you sure? because it's the wrong answer the right one is :\n1543\n------------------------------------------------------------------------"
                     L+=1
-                o2=raw_input("_The first rocket that successfully went to the moon?\n*Apolo11[a]\n*Manathan[m]\n*URUL[u]\n...")
-                if o2 == "a":
+                o2=raw_input("_The first rocket that successfully went to the moon?\n*Apolo11[1]\n*Manathan[2]\n*URUL[3]\n...")
+                if o2 == "1":
                     print"Success you are right!\n-------------------------------------------------------------------------"
                     K+=1
                 else:
                     print"Are you sure? because it's the wrong answer the right one is :\nApolo11\n------------------------------------------------------------------------"
                     L+=1
-                o3=raw_input("_What's the name of the robot that transports in Mars?\n*spacehub[s]\n*Rover[r]\n*discover213[d]\n...")
-                if o3 == "r":
+                o3=raw_input("_What's the name of the robot that transports in Mars?\n*spacehub[1]\n*Rover[2]\n*discover213[3]\n...")
+                if o3 == "2":
                     print"Success you are right!\n-------------------------------------------------------------------------"
                     K+=1
                 else:
@@ -89,29 +105,29 @@ while 1!=2:
                 else:
                     print"Are you sure? because it's the wrong answer the right one is :\n1958\n------------------------------------------------------------------------"
                     L+=1
-                o6=raw_input("_how many planets are there in the solar system?\n*8[8]\n*9[9]\n*10[10]\n...")
-                if o6 == "8":
+                o6=raw_input("_how many planets are there in the solar system?\n*8[1]\n*9[2]\n*10[3]\n...")
+                if o6 == "1":
                     print"Success you are right!\n-------------------------------------------------------------------------"
                     K+=1
                 else:
                     print"Are you sure? because it's the wrong answer the right one is :\n8\n------------------------------------------------------------------------"
                     L+=1
-                o7=raw_input("_who built the telescope?\n*Nutune[n]\n*George Alfury[g]\n*Hans Lippershey[h]\n...")
-                if o7 == "h":
+                o7=raw_input("_who built the telescope?\n*Nutune[1]\n*George Alfury[2]\n*Hans Lippershey[3]\n...")
+                if o7 == "3":
                     print"Success you are right!\n-------------------------------------------------------------------------"
                     K+=1
                 else:
                     print"Are you sure? because it's the wrong answer the right one is :\nHans Lippershey\n------------------------------------------------------------------------"
                     L+=1
-                o8=raw_input("_who knew that the earth is spinning around the sun?\n*Copernicus[c]\n*Shawn Mendes[s]\n*Lucuis Fox[l]...")
-                if o8 == "c":
+                o8=raw_input("_who knew that the earth is spinning around the sun?\n*Copernicus[1]\n*Shawn Mendes[2]\n*Lucuis Fox[3]\n...")
+                if o8 == "1":
                     print"Success you are right!\n-------------------------------------------------------------------------"
                     K+=1
                 else:
                     print"Are you sure? because it's the wrong answer the right one is :\nCopernicus\n------------------------------------------------------------------------"
                     L+=1
-                o9=raw_input("_who knew that the earth is a sphere planet?\nEratosthenes[e]\nFibonnacci[f]\nMarkossio[m]...")
-                if o9 == "e":
+                o9=raw_input("_who knew that the earth is a sphere planet?\nEratosthenes[1]\nFibonnacci[2]\nMarkossio[3]\n...")
+                if o9 == "1":
                     print"Success you are right!\n-------------------------------------------------------------------------"
                     K+=1
                 else:
@@ -120,6 +136,7 @@ while 1!=2:
                 print'the number of the right answers is: ', K
                 print'the number of the wrong answers is: ', L
     if Y == "G" or Y == "g":
+        print"In this game you will be limited by only 10 tries. This game is called the GuessGame"
         n = random.randint(1,100)
         x = input("choose a number from 0 to 100...")
         a = 0
@@ -127,19 +144,19 @@ while 1!=2:
             if a == 9:
                 print "game is over"
                 print "Loser, Loser :p"
-                print "number of tries: ", a+1
+                print "number of tries: ", a+1, "/10"
                 print n, "is the correct number"
                 raw_input()
                 break
             elif x<n:
                 print x, "is smaller"
                 a+=1
-                print "number of tries: ", a
+                print "number of tries: ", a, "/10"
                 x = input("choose a number again...")
             elif x>n:
                 print x, "is bigger"
                 a+=1
-                print "number of tries: ", a
+                print "number of tries: ", a, "/10"
                 x = input("choose a number again...")
         if n == x:
             print "yes", x, "is the correct number"
@@ -148,41 +165,26 @@ while 1!=2:
             raw_input()
     if Y == "C" or Y == "c":
         while 1!=2:
-            print "To try another operation when finished just press enter. LooooooooooooL XD"
-            x=raw_input("PS: To choose the + operation : +\nto choose the - operation : -\nto choose the x operation : x\nto choose / operation : /\nto choose the ^ operation : ^\nto go back : r\n... ")
-            if x == "r" or x == "R":
-                break
-            elif x == "+":
-                print "this is a + operation"
-                A = input("Please enter a valid number ")
-                B = input("Please enter a valid number ")
-                print A,"+", B, "=", A+B
-                raw_input()
-            elif x == "-":
-                print"this is a - oparation"
-                A1 = input("Please enter a valid number ")
-                B1 = input("Please enter a valid number ")
-                print A1,"-", B1, "=", A1-B1
-                raw_input()
-            elif x == "x":
-                print "This is a x operation"
-                A2 = input("Please enter a valid number ")
-                B2 = input("Please enter a valid number ")
-                print A2,"x", B2, "=", A2*B2
-                raw_input()
-            elif x == "/":
-                print "This is a / operation"
-                A3 = input("Please enter a valid number ")
-                B3 = input("Please enter a valid number ")
-                print A3,"/", B3, "=", A3/B3
-                raw_input()
-            elif x == "^":
-                print "This is a ^ operation"
-                A4 = input("Please enter a valid number ")
-                B4 = input("Please enter a valid number ")
-                print A4,"^", B4, "=", A4**B4
-                raw_input()
+            x = raw_input("to continue just press : c\nto go back just press: r\n...")
+            if x == "r":
+                break 
+            elif x == "c":
+                x1 = input("please enter a valid number\n...")
+                x2 = raw_input("choose from + or - or / or * or ^\n...")
+                x3 = input("please enter another valid number\n...")
+                if x2 == "+":
+                    print x1, "+", x3, "=", x1+x3
+                elif x2 == "-":
+                    print x1, "-", x3, "=", x1-x3
+                elif x2 == "/":
+                    print x1, "/", x3, "=", x1/x3
+                elif x2 == "*":
+                    print x1, "*", x3, "=", x1*x3
+                elif x2 == "^":
+                    print x1, "^", x3, "=", x1**x3
+                else:
+                    print "This doesn't exist!!!"
             else:
-                print "this doesn't existe!!!!!!!!"
-                raw_input()
+                exit()
+raw_input()
 
